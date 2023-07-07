f15bba64741cb5bef923d612632d189bf2d5de44
drapl <drapl@drapl.uk>
2023-07-07 15:06:53 +0100

    Add and Update functions added

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ screens = [
     {'id': 0,
      'name': 'Screen 1',
      'status': 'Down'},
-    {id: 1,
+    {'id': 1,
      'name': 'Screen 2',
      'status': 'Down'},
-    {id: 2,
+    {'id': 2,
      'name': 'Screen 3',
      'status': 'Down'}
 ]
@@ -21,4 +21,26 @@ screens = [
 def api_all():
     return jsonify(screens)
 
-app.run()
+@app.route('/api/v1/screens/add', methods=['POST'])
+def api_add():
+    if request.method == 'POST':
+
+        screens.append(request.get_json())
+        return "Done"
+
+@app.route('/api/v1/screens/update', methods=['POST'])
+def api_update():
+    if request.method == 'POST':
+
+        request_data = request.get_json()
+        updateId = request_data['id']
+        updateName = request_data['name']
+        updateStatus = request_data['status']
+
+        d = next(item for item in screens if item['id'] == updateId)
+        d['name'] = updateName
+        d['status'] = updateStatus
+
+        return "Done"
+
+app.run(debug=True)
